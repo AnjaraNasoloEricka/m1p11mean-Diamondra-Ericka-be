6c550ec925ca479d0f4e8e9b12136149d431dcb0
NasoloEricka <anjaranasoloericka@gmail.com>
2024-02-03 21:38:34 +0300

    [feat] init architecture skeleton

---
+++ services/ajv/ajvServices.js
@@ -0,0 +1,44 @@
+const Ajv = require('ajv');
+
+function validateSchema(schema, data) {
+    const ajv = new Ajv();
+    const valid = ajv.compile(schema);
+
+    if (!valid(data)) {
+        const errors = valid.errors.map(error => {
+            let errorMessage = '';
+            switch (error.keyword) {
+                case 'type':
+                    if (error.params.type === 'undefined') {
+                        errorMessage = `${error.instancePath.slice(1)} is required`;
+                    }
+                    break;
+                case 'required':
+                    errorMessage = `${error.instancePath.slice(1)} is required`;
+                    break;
+                case 'format':
+                    errorMessage = `${error.instancePath.slice(1)} is invalid`;
+                    break;
+                case 'pattern':
+                    errorMessage = `${error.instancePath.slice(1)} is invalid`;
+                    break;
+                case 'minLength':
+                    errorMessage = `${error.instancePath.slice(1)} is too short`;
+                    break;
+                case 'maxLength':
+                    errorMessage = `${error.instancePath.slice(1)} is too long`;
+                    break;
+                default:
+                    errorMessage = error.message;
+                    break;
+            }
+            return errorMessage;
+        });
+        const errorMessage = errors.join(', ');
+        throw new Error(errorMessage);
+    } 
+}
+
+module.exports = {
+    validateSchema,
+};
