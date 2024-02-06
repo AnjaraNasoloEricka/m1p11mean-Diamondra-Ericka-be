c30af75346d6271a538b4925fa7600eecf00c033
d.rambeloson <d.rambeloson@etechconsulting-mg.com>
2024-02-06 15:35:55 +0300

    [fix] Customer Schema

--- models/Customer.js
+++ models/Customer.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 
 const clientSchema = new mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        auto: true,
+    },
     user : {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
     },
-});
+    },
+    {
+        collection : 'customer'
+    });
 
-module.exports = mongoose.model('Client', clientSchema);;
+const Customer = mongoose.model('Customer', clientSchema);
+module.exports = { Customer };
