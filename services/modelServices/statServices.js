b3ada9c1a88336e3dd64fe624c267f5342e45eb3
NasoloEricka <anjaranasoloericka@gmail.com>
2024-02-29 16:20:50 +0300

    [feat] statServices template

---
+++ services/modelServices/statServices.js
@@ -0,0 +1,9 @@
+const statService = {
+    getCAperDay : async function(){
+        // get appointment.payment total amount where payment.date = date 
+               
+
+    }
+}
+
+module.exports = statService;
